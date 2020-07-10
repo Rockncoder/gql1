36132c77e6712840f42355125c06ee9e8f02e109
Troy Miles <rockncoder@gmail.com>
2020-07-09 22:42:06 -0700

    Removes simple queries, renders a list of vehicles

--- Query.cs
+++ Query.cs
@@ -8,27 +8,13 @@ namespace gql1
 {
     public class Query
     {
-        public Greetings GetGreetings() => new Greetings();
         public string HelloWorld() => "Hello";
-        public Character GetCharacter() => new Character();
         
-        // [UsePaging]
+        //[UsePaging]
         [UseFiltering]
         [UseSorting]
         public IQueryable<VehicleDto> GetVehicle() => AppState.Vehicles.AsQueryable();
     }
-
-    public class Greetings
-    {
-        public string Hello() => "World";
-    }
-
-    public class Character
-    {
-        public int Id { get; set; } = 1;
-        public string Name { get; set; } = "Luke";
-    }
 }
 
 
-// gatsby new epa_vehicles https://github.com/dominicabela/gatsby-starter-material-ui
---
+++ README.md
@@ -0,0 +1,11 @@
+# gql1
+## graphQL Example #1
+
+This program combines a GraphQL power ASP.NET Core API with a GatsbyJS/React/Material-UI frontend.
+
+This is a proof of concept, POC for fast page loads
+
+* Sub * 
+
+1. Show a vehicle
+2. Show a list of vehicles
